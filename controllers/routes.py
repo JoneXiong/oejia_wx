9875f4f71a734295f03d6cee9f2b9cdfbf02943b
jone@meilapp.com <jone@meilapp.com>
2015-10-25 20:43:04 +0800

    改善wx.config；添加微信服务wsgi

---
+++ controllers/routes.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+
+from mole import route, run
+import werobot
+robot = werobot.WeRoBot(token='K5Dtswpte', enable_session=True)
+robot.wsgi
+
+@route('/jone/mole')
+def index():
+    return 'Hello Mole!'
--- models/wx_config_model.py
+++ models/wx_config_model.py
@@ -3,18 +3,19 @@
 from openerp import models, fields, api
 
 
-class wx_config_settings(models.Model):
+class wx_config_settings(models.TransientModel):
     _name = 'wx.config.settings'
     _description = u'微信配置'
     #_order = 
     _inherit = 'res.config.settings'
 
-    wx_AccessToken = fields.Char('当前AccessToken', )
-    wx_AppSecret = fields.Char('AppSecret', )
-    
     wx_appid = fields.Char('AppId', )
-    wx_token = fields.Char('Token', )
+    wx_AppSecret = fields.Char('AppSecret', )
+    wx_AccessToken = fields.Char('当前AccessToken', )
+    
     wx_url = fields.Char('URL', )
+    wx_token = fields.Char('Token', )
+
 
     #_defaults = {
     #}
