2763fa3af232c38947c7d0d7521fcb52ad5c2f69
jone@meilapp.com <jone@meilapp.com>
2015-10-30 12:01:00 +0800

    wx 自动回复加入模糊匹配和正则匹配的支持

--- controllers/handlers/auto_reply.py
+++ controllers/handlers/auto_reply.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+import re
 
 from ..routes import robot
 from openerp.http import request
@@ -13,6 +14,15 @@ def input_handle(message, session):
     for rc in rs:
         if rc.type==1:
             if content==rc.key:
+                return rc.action.get_wx_reply()
+        elif rc.type==2:
+            if rc.key in content:
+                return rc.action.get_wx_reply()
+        elif rc.type==3:
+            try:
+                flag = re.compile(rc.key).match(content)
+            except:flag=False
+            if flag:
                 return rc.action.get_wx_reply()
     
     if content.startswith('e'):
