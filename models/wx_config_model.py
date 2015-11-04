a72214afb52e67dae1ec415be0ad1959b683a01e
jone@meilapp.com <jone@meilapp.com>
2015-11-04 22:58:42 +0800

    wx app 相关默认值修缮

--- models/wx_config_model.py
+++ models/wx_config_model.py
@@ -39,9 +39,9 @@ class wx_config_settings(models.TransientModel):
     def get_default_wx_appid(self, cr, uid, fields, context=None):
         Param = self.pool.get("ir.config_parameter")
         return {
-                'wx_appid': Param.get_param(cr, uid, 'wx_appid', context=context) or '',
-                'wx_AppSecret': Param.get_param(cr, uid, 'wx_AppSecret', context=context) or '',
-                'wx_token': Param.get_param(cr, uid, 'wx_token', context=context) or '',
+                'wx_appid': Param.get_param(cr, uid, 'wx_appid', default='appid_xxxxxxxxxxxxxxx', context=context),
+                'wx_AppSecret': Param.get_param(cr, uid, 'wx_AppSecret', default='appsecret_xxxxxxxxxxxxxx', context=context),
+                'wx_token': Param.get_param(cr, uid, 'wx_token', default='token_xxxxxxxx', context=context),
                 }
     
     def set_wx_appid(self, cr, uid, ids, context=None):
