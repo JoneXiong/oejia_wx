097a642f58f3bcc49e698c77232d5d4bca7f4681
jone@meilapp.com <jone@meilapp.com>
2015-11-06 02:56:42 +0800

    关注与取消关注时用户存储的处理

--- controllers/handlers/sys_event.py
+++ controllers/handlers/sys_event.py
@@ -1,6 +1,8 @@
 # coding=utf-8
 
 from ..routes import robot
+from .. import client
+from openerp.http import request
 
 
 @robot.subscribe
@@ -8,14 +10,25 @@ def subscribe(message):
     serviceid = message.target
     openid = message.source
 
-    return "您终于来了！欢迎关注可友电话"
+    info = client.wxclient.get_user_info(openid)
+    info['group_id'] = str(info['groupid'])
+    env = request.env()
+    rs = env['wx.user'].sudo().search( [('openid', '=', openid)] )
+    if not rs.exists():
+        env['wx.user'].sudo().create(info)
+    
+    return "您终于来了！欢迎关注"
 
 @robot.unsubscribe
 def unsubscribe(message):
     
     serviceid = message.target
     openid = message.source
-
+    env = request.env()
+    rs = env['wx.user'].sudo().search( [('openid', '=', openid)] )
+    if rs.exists():
+        rs.unlink()
+                        
     return "欢迎下次光临！"
     
 @robot.view
