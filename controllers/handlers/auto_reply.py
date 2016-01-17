a516ff53ca418528ebb1023fedc020f8c5159bd9
root <root@iZ23vhs7xarZ.(none)>
2016-01-17 16:19:28 +0800

    微信对接bus可行fix

--- controllers/handlers/auto_reply.py
+++ controllers/handlers/auto_reply.py
@@ -13,7 +13,7 @@ def input_handle(message, session):
     serviceid = message.target
     openid = message.source
     
-    rs = request.env()['wx.autoreply'].search([])
+    rs = request.env()['wx.autoreply'].sudo().search([])
     for rc in rs:
         if rc.type==1:
             if content==rc.key:
@@ -29,24 +29,27 @@ def input_handle(message, session):
                 return rc.action.get_wx_reply()
     #客服对话
     uuid = session.get("uuid", None)
+    ret_msg = ''
     cr, uid, context, db = request.cr, request.uid or openerp.SUPERUSER_ID, request.context, request.db
     registry = request.env()
     if not client.UUID_OPENID.has_key(db):
         client.UUID_OPENID[db] = {}
     if not uuid:
-        channel_id = 1
+        channel_id = 2
         info = client.wxclient.get_user_info(openid)
         anonymous_name = info.get('nickname','微信网友')
-        session_info = registry['im_livechat.channel'].get_channel_session(cr, uid, channel_id, anonymous_name, context=context)
-        uuid = session_info['uuid']
+	reg = openerp.modules.registry.RegistryManager.get(db)
+        session_info = reg.get('im_livechat.channel').get_channel_session(cr, uid, channel_id, anonymous_name, context=context)
+	uuid = session_info['uuid']
         session["uuid"] = uuid
+	ret_msg = '请稍后，正在分配客服为您解答'
     client.UUID_OPENID[db][uuid] = openid
 
     message_type = "message"
     message_content = content
     registry, cr, uid, context = request.registry, request.cr, request.session.uid, request.context
-    message_id = registry["im_chat.message"].post(cr, openerp.SUPERUSER_ID, uid, uuid, message_type, message_content, context=context)
-    return '请稍后，正在分配客服为您解答'
+    message_id = registry["im_chat.message"].post(cr,openerp.SUPERUSER_ID,uid, uuid, message_type, message_content, context=context)
+    return ret_msg
     
     if content.startswith('e'):
         return content
