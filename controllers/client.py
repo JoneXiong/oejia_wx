4f8debcc7bb023312d07ead2539941614c691177
jone@meilapp.com <jone@meilapp.com>
2016-04-05 13:14:55 +0800

    客服消息发送失败时的异常处理

--- controllers/client.py
+++ controllers/client.py
@@ -1,13 +1,17 @@
 # coding=utf-8
 
-from werobot.client import Client
+from werobot.client import Client, ClientException
+from openerp import exceptions
 
 wxclient = Client('appid_xxxxxxxxxxxxxxx', 'appsecret_xxxxxxxxxxxxxx')
 
 UUID_OPENID = {}
 
 def send_text(openid,text):
-    wxclient.send_text_message(openid, text)
+    try:
+        wxclient.send_text_message(openid, text)
+    except ClientException, e:
+        raise exceptions.UserError(u'发送失败 %s'%e)
 
 def chat_send(db,uuid, msg):
     _dict = UUID_OPENID.get(db,None)
