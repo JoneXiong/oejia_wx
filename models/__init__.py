b450d8377313b9143f72ec2d8cff70d130e780d1
xiongjianhong@gmail.com <xiongjianhong@gmail.com>
2018-03-11 15:25:11 +0800

    update for new version 11

--- models/__init__.py
+++ models/__init__.py
@@ -5,7 +5,6 @@ from menu_about_models import *
 from user_models import *
 from wx_autoreply_model import *
 from wx_config_model import *
-from chat_message import *
 from res_partner import *
 from mail_message import *
 
