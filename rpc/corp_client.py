94ed75d60e89e6b12d5f64e60fe62ab928883149
xiongjianhong@gmail.com <xiongjianhong@gmail.com>
2018-04-15 22:07:38 +0800

    优化corp client初始化的管理

--- rpc/corp_client.py
+++ rpc/corp_client.py
@@ -1,6 +1,12 @@
 # coding=utf-8
+import logging
 
 from wechatpy.enterprise import WeChatClient
+
+_logger = logging.getLogger(__name__)
+
+
+crypto_handle = None
 
 # 用于agent应用API交互的client
 client = None
@@ -33,9 +39,39 @@ def init_txl_client(appid, secret):
     return txl_client
 
 def chat_send(db,uuid, msg):
-    _dict = UUID_OPENID.get(db,None)
-    if _dict:
-        openid = _dict.get(uuid,None)
+    #_dict = UUID_OPENID.get(db,None)
+    if UUID_OPENID:
+        openid = UUID_OPENID.get(uuid,None)
         if openid:
             client.message.send_text(current_agent, openid, msg)
     return -1
+
+def init(env):
+    Param = env['ir.config_parameter'].sudo()
+
+    Corp_Token = Param.get_param('Corp_Token') or ''
+    Corp_AESKey = Param.get_param('Corp_AESKey') or ''
+
+    Corp_Id = Param.get_param('Corp_Id') or ''       # 企业号
+    Corp_Secret = Param.get_param('Corp_Secret') or ''
+    Corp_Agent = Param.get_param('Corp_Agent') or ''
+
+    from wechatpy.enterprise.crypto import WeChatCrypto
+    global current_agent
+    global crypto_handle
+    _logger.info('Create crypto: %s %s %s'%(Corp_Token, Corp_AESKey, Corp_Id))
+    try:
+        crypto_handle = WeChatCrypto(Corp_Token, Corp_AESKey, Corp_Id)
+    except:
+        _logger.error('初始化微信客户端实例失败，请在微信对接配置中填写好相关信息！')
+    init_client(Corp_Id, Corp_Secret)
+    init_txl_client(Corp_Id, Corp_Secret)
+    current_agent = Corp_Agent
+
+    global OPENID_UUID
+    global UUID_OPENID
+    users = env['wx.corpuser'].sudo().search([('last_uuid','!=',None)])
+    for obj in users:
+        OPENID_UUID[obj.userid] = obj.last_uuid
+        UUID_OPENID[obj.last_uuid] = obj.userid
+    print('corp client init: %s %s'%(OPENID_UUID, UUID_OPENID))
