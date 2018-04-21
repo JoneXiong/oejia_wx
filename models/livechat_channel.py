40849f038263ce791e257a1f03aa1b15457712d9
xiongjianhong@gmail.com <xiongjianhong@gmail.com>
2018-04-22 02:18:25 +0800

    聊天客服消息优化

--- models/livechat_channel.py
+++ models/livechat_channel.py
@@ -12,6 +12,9 @@ class LivechatChannel(models.Model):
 
     _inherit = 'im_livechat.channel'
 
+    @api.model
+    def create_mail_channel(self, livechat_channel_id, anonymous_name, content):
+        return self.get_mail_channel(livechat_channel_id, anonymous_name), self.get_wx_default_msg()
 
     @api.model
     def get_wx_default_msg(self):
