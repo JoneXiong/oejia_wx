428c16760487c1d44628c7ada623ad0ae121176a
xiongjianhong@gmail.com <xiongjianhong@gmail.com>
2018-06-25 12:28:12 +0000

    odoo聊天支持图片消息

--- controllers/handlers/auto_reply.py
+++ controllers/handlers/auto_reply.py
@@ -1,6 +1,8 @@
 # coding=utf-8
 import re
 import logging
+import base64
+import os
 
 from openerp.http import request
 import openerp
@@ -8,9 +10,25 @@ from .. import client
 
 _logger = logging.getLogger(__name__)
 
+
+def get_img_data(pic_url):
+    import requests
+    headers = {
+	'Accept': 'textml,application/xhtml+xml,application/xml;q=0.9,image/webp,/;q=0.8',
+	'Accept-Encoding': 'gzip, deflate',
+	'Accept-Language': 'zh-CN,zh;q=0.8,en;q=0.6,zh-TW;q=0.4',
+	'Cache-Control': 'no-cache',
+	'Host': 'mmbiz.qpic.cn',
+	'Pragma': 'no-cache',
+	'Connection': 'keep-alive',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+    }
+    r = requests.get(pic_url,headers=headers,timeout=50)
+    return r.content
+
 def main(robot):
 
-    @robot.text
+    #@robot.text
     def input_handle(message, session):
         from .. import client
         entry = client.wxenv(request.env)
@@ -18,7 +36,21 @@ def main(robot):
         content = message.content.lower()
         serviceid = message.target
         openid = message.source
-        _logger.info('>>> wx text msg: %s'%content)
+        _logger.info('>>> wx msg: %s'%message)
+        #_logger.info('>>> wx text msg: %s'%content)
+        pic_url = message.img
+        attachment_ids = []
+        if pic_url:
+            _data = get_img_data(pic_url)
+            _filename = os.path.basename(pic_url)
+            attachment = Model.create({
+                'name': _filename,
+                'datas': base64.encodestring(_data),
+                'datas_fname': _filename,
+                'res_model': 'mail.compose.message',
+                'res_id': int(0)
+            })
+            attachment_ids.append(attachment.id)
 
         rs = request.env()['wx.autoreply'].sudo().search([])
         for rc in rs:
@@ -68,6 +100,8 @@ def main(robot):
             if request.session.uid:
                 author_id = request.env['res.users'].sudo().browse(request.session.uid).partner_id.id
             mail_channel = request.env["mail.channel"].sudo(request_uid).search([('uuid', '=', uuid)], limit=1)
-            message = mail_channel.sudo(request_uid).with_context(mail_create_nosubscribe=True).message_post(author_id=author_id, email_from=False, body=message_content, message_type='comment', subtype='mail.mt_comment', content_subtype='plaintext')
+            message = mail_channel.sudo(request_uid).with_context(mail_create_nosubscribe=True).message_post(author_id=author_id, email_from=False, body=message_content, message_type='comment', subtype='mail.mt_comment', content_subtype='plaintext',attachment_ids=attachment_ids)
 
         return ret_msg
+    robot.add_handler(input_handle, type='text')
+    robot.add_handler(input_handle, type='image')
