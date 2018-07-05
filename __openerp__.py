30c513a3daa2a9be82ccea68b4736726971f4e2e
xiongjianhong@gmail.com <xiongjianhong@gmail.com>
2018-07-05 02:45:37 +0000

    增加权限和角色组的定义

--- __openerp__.py
+++ __openerp__.py
@@ -10,6 +10,8 @@
     'application': True,
     'auto_data_include': ['views'],
     'data': [
+             'security/ir.model.access.csv',
+             'security/res_groups.xml',
              'data/wx_init_data.xml',
              'views/parent_menus.xml',
 
