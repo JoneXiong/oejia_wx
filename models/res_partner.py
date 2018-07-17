b0f6c689b962650adbfd9ad5a9d339eb1e9f9c97
xiongjianhong@gmail.com <xiongjianhong@gmail.com>
2018-07-17 11:15:07 +0800

    空格fix

--- models/res_partner.py
+++ models/res_partner.py
@@ -5,5 +5,5 @@ from openerp import models, fields, api
 
 class res_partner(models.Model):
     _inherit = 'res.partner'
-    
+
     wxcorp_user_id = fields.Many2one('wx.corpuser','关联企业号用户')
--- models/wx_autoreply_model.py
+++ models/wx_autoreply_model.py
@@ -10,12 +10,12 @@ class wx_autoreply(models.Model):
     _description = u'自动回复'
     #_order = 
     #_inherit = []
-    
+
     key = fields.Char('匹配内容', )
     type = fields.Selection( [(1,'完全匹配'),(2,'模糊匹配'),(3,'正则匹配')], '匹配方式', )
     action = fields.Reference(string='动作', selection=ACTION_OPTION)
     sequence = fields.Integer('匹配顺序', help="数字越小越先匹配")
-    
+
     _defaults = {
                  'type': 1,
                  'sequence': 0
