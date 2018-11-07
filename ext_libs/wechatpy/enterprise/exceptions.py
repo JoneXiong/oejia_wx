014aa750b41451daf906abc8b4a8846b025d9f4d
xiongjianhong@gmail.com <xiongjianhong@gmail.com>
2018-11-07 23:39:02 +0800

    rm lib wechatpy

--- ext_libs/wechatpy/enterprise/exceptions.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import absolute_import, unicode_literals
-from wechatpy.exceptions import WeChatException
-
-
-class InvalidCorpIdException(WeChatException):
-
-    def __init__(self, errcode=-40005, errmsg='Invalid corp_id'):
-        super(InvalidCorpIdException, self).__init__(errcode, errmsg)
