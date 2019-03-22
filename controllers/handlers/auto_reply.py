4f6122684fddbb03544d09ee06a2763ed55d77ee
xiongjianhong@gmail.com <xiongjianhong@gmail.com>
2019-03-22 18:20:48 +0800

    create_reply优化 统一到entry；关注后自动消息优化加强 支持配置各种消息类型

--- controllers/handlers/auto_reply.py
+++ controllers/handlers/auto_reply.py
@@ -5,20 +5,12 @@ import base64
 import os
 import datetime
 
-from werobot.reply import create_reply as create_reply_init
 from openerp.http import request
 import openerp
 from .. import client
 
 _logger = logging.getLogger(__name__)
 
-def create_reply(ret_msg, message=None, entry=None):
-    if hasattr(ret_msg, '_name'):
-        if entry:
-            entry.wxclient.send_news_message(message.source, ret_msg.media_id)
-        return None
-    else:
-        return create_reply_init(ret_msg, message=message)
 
 def get_img_data(pic_url):
     import requests
@@ -87,18 +79,18 @@ def main(robot):
             if rc.type==1:
                 if content==_key:
                     ret_msg = rc.action.get_wx_reply()
-                    return create_reply(ret_msg, message=message, entry=entry)
+                    return entry.create_reply(ret_msg, message)
             elif rc.type==2:
                 if _key in content:
                     ret_msg = rc.action.get_wx_reply()
-                    return create_reply(ret_msg, message=message, entry=entry)
+                    return entry.create_reply(ret_msg, message)
             elif rc.type==3:
                 try:
                     flag = re.compile(_key).match(content)
                 except:flag=False
                 if flag:
                     ret_msg = rc.action.get_wx_reply()
-                    return create_reply(ret_msg, message=message, entry=entry)
+                    return entry.create_reply(ret_msg, message)
         #客服对话
         uuid, record_uuid = entry.get_uuid_from_openid(openid)
         ret_msg = ''
@@ -134,7 +126,7 @@ def main(robot):
             mail_channel = request.env["mail.channel"].sudo(request_uid).search([('uuid', '=', uuid)], limit=1)
             msg = mail_channel.sudo(request_uid).with_context(mail_create_nosubscribe=True).message_post(author_id=author_id, email_from=mail_channel.anonymous_name, body=message_content, message_type='comment', subtype='mail.mt_comment', content_subtype='plaintext',attachment_ids=attachment_ids)
         if ret_msg:
-            return create_reply(ret_msg, message=message)
+            return entry.create_reply(ret_msg, message)
     robot.add_handler(input_handle, type='text')
     robot.add_handler(input_handle, type='image')
     robot.add_handler(input_handle, type='voice')
--- controllers/handlers/sys_event.py
+++ controllers/handlers/sys_event.py
@@ -10,19 +10,22 @@ def main(robot):
     def subscribe(message):
         from .. import client
         entry = client.wxenv(request.env)
-        client = entry
         serviceid = message.target
         openid = message.source
 
-        info = client.wxclient.get_user_info(openid)
+        info = entry.wxclient.get_user_info(openid)
         info['group_id'] = str(info['groupid'])
         env = request.env()
         rs = env['wx.user'].sudo().search( [('openid', '=', openid)] )
         if not rs.exists():
             env['wx.user'].sudo().create(info)
 
-        ret_msg = "您终于来了！欢迎关注"
-        return create_reply(ret_msg, message=message)
+        if entry.subscribe_auto_msg:
+            ret_msg = entry.subscribe_auto_msg
+        else:
+            ret_msg = "您终于来了！欢迎关注"
+
+        return entry.create_reply(ret_msg, message)
 
     @robot.unsubscribe
     def unsubscribe(message):
