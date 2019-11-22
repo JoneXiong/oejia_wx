7a5307bb032ec158799aa6528d904a74990567d7
xiongjianhong@gmail.com <xiongjianhong@gmail.com>
2019-11-22 17:18:33 +0800

    odoo13 patch fix

--- od13.py
+++ od13.py
@@ -1,3 +1,6 @@
+import logging
+_logger = logging.getLogger(__name__)
+
 def multi(method):
     return method
 from odoo import api
@@ -9,6 +12,12 @@ api.multi = multi
 from odoo import models
 origin_write = models.Model.write
 def write(self, vals):
-    vals = [e for e in vals if e in self._fields]
-    return origin_write(self, vals)
+    _vals = {}
+    for k,v in vals.items():
+        if k in self._fields:
+            _vals[k] = v
+        else:
+            _logger.warning('>>> odoo 13 hook: model %s has no field %s', self._name, k)
+    #vals = { k:v for k,v in vals.items() if k in self._fields}
+    return origin_write(self, _vals)
 models.Model.write = write
